4d4614cc42cf0675593b7990b4c53427057b9ef4
nozagleh <nozagleh@gmail.com>
2015-03-20 00:30:52 +0100

    added smooth scroll

--- jquery.js
+++ jquery.js
@@ -1,6 +1,12 @@
 $(function(){
-
-	
+	$('a').click(function(){
+				$('html, body').animate({
+					scrollTop: $( $.attr(this, 'href') ).offset().top - 120
+				}, 500);
+				return false;
+			});
+	$('.boxtext').hide();
+	$('.boxtext').fadeIn(1000);	
 
 	$(window).scroll(function(){
 		var $scroll = $(window).scrollTop();
