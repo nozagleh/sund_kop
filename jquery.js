8aba705b2120d29287f68d5c6aab53e398b15100
nozagleh <nozagleh@gmail.com>
2015-03-20 02:06:51 +0100

    change background images

--- jquery.js
+++ jquery.js
@@ -7,7 +7,9 @@ $(function(){
 			});
 	$('.boxtext').hide();
 	$('.boxtext').fadeIn(1000);	
+	$('.tt').hide();
 
+	var $showtt = $('.mtop').offset().top + 300;
 	$(window).scroll(function(){
 		var $scroll = $(window).scrollTop();
 		if ($scroll >20) {
@@ -16,6 +18,11 @@ $(function(){
 		}else{
 			$('.header').removeClass('smallnav');
 			$('.container').css({'margin-top': '0px'});
+		};
+		if ($scroll > $showtt) {
+			$('.tt').fadeIn(500);	
+		}else {
+			$('.tt').fadeOut(1000).hide();
 		};
 	});
 });
